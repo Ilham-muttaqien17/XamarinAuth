35f5e26c8ed4e4970356252ea6ebc026365cf608
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-01-04 20:36:27 +0700

    Add project files.

---
+++ Model/TbUser.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamarinAuth.Model
+{
+    public class TbUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+    }
+}
